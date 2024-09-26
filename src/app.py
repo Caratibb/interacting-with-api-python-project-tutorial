78164d9aaf58801ec3673d65ed2e066bfb74a91f
Caratibb <caratibbetts19@gmail.com>
2024-09-26 00:03:03 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -5,3 +5,10 @@ from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
